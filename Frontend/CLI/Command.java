1b9a5f4327923c390e4a4dad0879cf9c2f4cb016
Shokora <shokora@gmail.com>
2009-09-15 15:04:53 +0200

    first commit

--- Frontend/CLI/Command.java
+++ Frontend/CLI/Command.java
@@ -5,22 +5,26 @@ import java.util.*;
 /**
  *
  * @author shokora
+ * @todo Remove the argcount and add a list of parameters the command has.
+ * These parameters will be added to a list and the command call will be checked for
+ * an instance of these parameters. Then the list of parameters will be send to the run command
+ * so the run command itself doesn't have to do any parameter checking.
  */
 public abstract class Command
 {
     private int argCount;
-    private String description, token;
+    private String description, name;
 
-    public Command(String token, int argCount, String description)
+    public Command(String name, int argCount, String description)
     {
-        this.token = token;
+        this.name = name;
         this.argCount = argCount;
         this.description = description;
     }
 
     public String getToken()
     {
-        return token;
+        return name;
     }
 
     public int getArgCount()
@@ -35,7 +39,7 @@ public abstract class Command
 
     public boolean validizeCall(String token, ArrayList<String> args) //variable amount of arguments
     {
-        return this.token.equals(token) && (argCount == -1 || args.size() == argCount);
+        return this.name.equals(token) && (argCount == -1 || args.size() == argCount);
     }
 
     public abstract void run(ArrayList<String> args);
